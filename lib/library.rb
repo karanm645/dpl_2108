bcbd12617e9059c259e8545c9ff0c96f0ae2cc8f
Karan Mehta <karanm645@gmail.com>
2021-09-17 11:05:11 -0500

    Iteration 4 solutions

--- lib/library.rb
+++ lib/library.rb
@@ -1,16 +1,66 @@
 class Library
   attr_reader :name,
               :books,
-              :authors
+              :authors,
+              :checked_out_books
 
   def initialize(name)
     @name    = name
-    @books   = []
+    @books   = [] #available books
     @authors = []
+    @checked_out_books = [] #unavailable books
+    @checkout_count = Hash.new(0) #we put 0 so we could default to 0 because we are keeping track of it
   end
 
   def add_author(author)
+    #dani's way below
+    author.books.each do |book|
+      @books << book
+    end
     @authors << author
-    @books += author.books
+    #my way below
+    # @authors << author
+    # @books += author.books
+  end
+
+  #dani's way below
+  def publication_time_frame_for(author)
+    sorted_books = author.books.sort_by do |book|
+      book.publication_year
+    end
+    {
+      start: sorted_books.first.publication_year,
+      end: sorted_books.last.publication_year
+    }
+  end
+
+  #@books is available books in library
+  def checkout(requested_book)
+    if @books.include?(requested_book)
+      @checked_out_books << requested_book
+      @checkout_count[requested_book] += 1
+      @books.delete(requested_book)
+      true #if it is available it returns true and mvoes to unavailable)
+    else
+      false #if it isn't avalable (so in checked_out_books or doesnt even exists)
+  end
+
+  def return(checked_out_book)
+    if @checked_out_books.include?(checked_out_book)
+      @books << checked_out_books
+      @checked_out_books.delete(checked_out_books)
+  end
+  #book can be key and the times its checked out can be the value
+  def most_popular_book
+    #the book to be my key and the ties checkout to be the value
+    popular = ''
+    max = 0
+    @checkeout_count.each do |book, count|
+      if count > max
+        max = count
+        popular = book
+      end
+    end
+    popular #storing the book object
   end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -19,11 +19,10 @@ RSpec.describe Library do
 
   it "add_author" do
     charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
     jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
     professor = charlotte_bronte.write("The Professor", "1857")
     villette = charlotte_bronte.write("Villette", "1853")
-
-    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
     mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
 
     charlotte_bronte.publish(jane_eyre)
@@ -35,8 +34,73 @@ RSpec.describe Library do
     @dpl.add_author(charlotte_bronte)
     @dpl.add_author(harper_lee)
 
-
     expect(@dpl.authors).to eq([charlotte_bronte, harper_lee])
     expect(@dpl.books).to eq([jane_eyre, professor, villette, mockingbird])
+  end
+  it "" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    charlotte_bronte.publish(jane_eyre)
+    charlotte_bronte.publish(professor)
+    charlotte_bronte.publish(villette)
+
+    harper_lee.publish(mockingbird)
+
+    @dpl.add_author(charlotte_bronte)
+    @dpl.add_author(harper_lee)
+
+    expect(dpl.publication_time_frame_for(charlotte_bronte)).to eq({:start=>"1847", :end=>"1857"})
+    expect(dpl.publication_time_frame_for(harper_lee)).to eq({:start=>"1960", :end=>"1960"})
+  it "" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    charlotte_bronte.publish(jane_eyre)
+    charlotte_bronte.publish(professor)
+    charlotte_bronte.publish(villette)
+
+    harper_lee.publish(mockingbird)
+
+    @dpl.add_author(charlotte_bronte)
+    @dpl.add_author(harper_lee)
+    @dpl.add_author(charlotte_bronte)
+    @dpl.add_author(harper_lee)
+    @dpl.checkout(jane_eyre)
+
+    expect(dpl.checkout(mockingbird)).to eq(false)
+    expect(dpl.checkout(jane_eyre)).to eq(false)
+  end
+  it "" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    charlotte_bronte.publish(jane_eyre)
+    charlotte_bronte.publish(professor)
+    charlotte_bronte.publish(villette)
+
+    harper_lee.publish(mockingbird)
+
+    @dpl.add_author(charlotte_bronte)
+    @dpl.add_author(harper_lee)
+    @dpl.add_author(charlotte_bronte)
+    @dpl.add_author(harper_lee)
+    
+    @dpl.checkout(jane_eyre)
+
+    expect(dpl.checkout(mockingbird)).to eq(false)
+    expect(dpl.checkout(jane_eyre)).to eq(false)
   end
 end
